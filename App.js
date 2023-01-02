1c060f5c509ca678755e26073711f3fbdc235453
shashiofficialaccount <shashiofficialmail@gmail.com>
2023-01-02 17:03:43 +0530

    Updated

--- App.js
+++ App.js
@@ -1,5 +1,22 @@
-const heading = React.createElement("h1", {}, "Namaste EveryOne");
+import React from "react";
+import ReactDOM, { createRoot } from "react-dom/client";
 
+const Title = () => (
+  <h1 id="title" key="h2">
+    Namaste React
+  </h1>
+);
+
+// Composing Comopnentss
+const HeaderComponent = () => {
+  return (
+    <div>
+      <Title />
+      <h2>Namaste React functional component</h2>
+      <h2>This is a h2 tage</h2>
+    </div>
+  );
+};
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(<HeaderComponent />);
